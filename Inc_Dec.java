968a62c2b03aa0d0a41ecb1770b16f6040815b39
Sharvari Mehta <36322980+msharvari@users.noreply.github.com>
2025-02-21 21:18:32 +0530

    Update Inc_Dec.java

--- Inc_Dec.java
+++ Inc_Dec.java
@@ -3,7 +3,6 @@ package Harry;
 public class Inc_Dec {
 	public static void main(String[] args) {
 	float a = 7/4.0f * 9/2.0f;
-	
 	System.out.println(a);
 	int b = 7*49/7 + 35/7 ;
 	System.out.println(b);
